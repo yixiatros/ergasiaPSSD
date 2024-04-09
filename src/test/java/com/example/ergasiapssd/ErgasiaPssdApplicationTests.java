548ca9566d1dfcd231ee36304bae67d68a847223
yixiatros <alexandrosdimitriadis02@gmail.com>
2024-04-09 16:36:12 +0300

    v0.0

---
+++ src/test/java/com/example/ergasiapssd/ErgasiaPssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ergasiapssd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgasiaPssdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
